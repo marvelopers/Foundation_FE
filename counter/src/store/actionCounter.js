9a58cd41a3b42bdc966d29592a07fe9814dcfbfb
marvelopers <marvelopers@gmail.com>
2020-06-23 19:36:17 +0900

    Redux-saga-log

--- counter/src/store/actionCounter.js
+++ counter/src/store/actionCounter.js
@@ -5,11 +5,18 @@ const status = {
 //액션에 따른 실행 함수
 export default function counterReducer(state = status, action) {
   switch (action.type) {
-    case "DECREMENT_REQUEST":
-
-      return { number: action.payload - 1 };
     case "INCREMENT_REQUEST":
+      console.log('3-1. Counter.jsx => dispatch => actionCounter.js => case ');
+      return state;
+    case "INCREMENT":
+      console.log('6-1. actionCounter => incerment');
       return { number: action.payload + 1 };
+    case "DECREMENT_REQUEST":
+      console.log('3-2. Counter.jsx => dispatch => actionCounter.js => case ');
+      return state;
+    case "DECREMENT":
+      console.log('6-2. actionCounter => decrement');
+      return { number: action.payload - 1 };
     default: return state;
   }
 }
